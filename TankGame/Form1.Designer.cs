5fa6da41d4601f09b53901c3f5b803f804e0b585
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-05-05 23:14:13 +0300

    Add project files.

---
+++ TankGame/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace TankGame
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.player = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 50;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// player
+			// 
+			this.player.BackColor = System.Drawing.Color.Transparent;
+			this.player.BackgroundImage = global::TankGame.Properties.Resources.background_white;
+			this.player.Image = global::TankGame.Properties.Resources.player_1_;
+			this.player.Location = new System.Drawing.Point(30, 92);
+			this.player.Name = "player";
+			this.player.Size = new System.Drawing.Size(77, 83);
+			this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.player.TabIndex = 0;
+			this.player.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = global::TankGame.Properties.Resources.background_white;
+			this.ClientSize = new System.Drawing.Size(734, 411);
+			this.Controls.Add(this.player);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+			((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox player;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ TankGame/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TankGame
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		bool up, down, right;
+
+		void player_move()
+		{
+			if(up == true)
+			{
+				if(player.Top > 50)
+				{
+					player.Top -= 5;
+				}
+			}
+			if(down == true) 
+			{
+				if(player.Top < 220) 
+				{
+					player.Top += 5;
+				}
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			player_move();
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.Up: up = true; break;
+				case Keys.Down: down = true; break;
+				case Keys.Right: right = true; break;
+			}
+		}
+
+		private void Form1_KeyUp(object sender, KeyEventArgs e)
+		{
+			switch (e.KeyCode)
+			{
+				case Keys.Up: up = false; break;
+				case Keys.Down: down = false; break;
+				case Keys.Right: right = false; break;
+			}
+		}
+	}
+}
